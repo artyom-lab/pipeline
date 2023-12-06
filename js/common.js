ba2c0026081a831fa2036a25398509a9ccc59596
artyom-lab <artyomgalkeane@gmail.com>
2023-12-06 12:33:07 +0300

    0612

--- js/common.js
+++ js/common.js
@@ -25,25 +25,6 @@ $(document).ready(function () {
 
   $('.select-beast').selectize({});
 
-  autosize();
-  function autosize(){
-    var text = $('.autosize');
-
-    text.each(function(){
-        $(this).attr('rows',1);
-        resize($(this));
-    });
-
-    text.on('input', function(){
-        resize($(this));
-    });
-    
-    function resize ($text) {
-        $text.css('height', 'auto');
-        $text.css('height', $text[0].scrollHeight+'px');
-    }
-  };
-
  // SELECT2
 
 (function($) {
